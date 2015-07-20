6e5e52b0052d03180dbcf982e0720e9e1cf08a37
Jacob Mitchell <jmitchell@member.fsf.org>
2015-07-19 17:15:10 -0700

    Default emacs config.
    Sets sane global defaults, updates package repos, and installs SLIME and
    magit.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,36 @@
 ;;;; init.el
+
+
+;; global defaults
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(load-theme 'wombat)
+
+(show-paren-mode 1)
+
+(setq inhibit-startup-message t)
+(setq initial-scractch-message "")
+
+(setq-default indent-tabs-mode nil)
+
+
+;; packages
+(require 'package)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+(unless package-archive-contents
+    (package-refresh-contents))
+
+
+;; slime
+(package-install 'slime)
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+
+;; magit
+(package-install 'magit)
